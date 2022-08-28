ae74ee29f355271535d585be41e6a31eafc63557
ksuzuki-1 <ksuzuki5145@gmail.com>
2022-08-29 00:32:03 +0900

    setup_navigation

--- app/src/main/java/com/example/permissiondispatcherktxdemo/MainActivity.kt
+++ app/src/main/java/com/example/permissiondispatcherktxdemo/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.permissiondispatcherktxdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
